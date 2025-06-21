15fe422b835a7a745e0ab59912215d6ec81de7f9
kashubhatkar <bhatkarkahkasha@gmail.com>
2025-06-21 22:27:01 +0530

    conversion

---
+++ operations.js
@@ -0,0 +1,11 @@
+let str = "33.5";          // string
+console.log(typeof str);   // "string"
+
+// Convert string to number
+let num = Number(str);
+console.log(typeof num);   // "number"
+console.log(num);          // 33.5
+
+// Now you can do operations with the number
+let result = num + 10;
+console.log(result);       // 43.5
