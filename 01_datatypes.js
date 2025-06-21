32e7aeb30e94dd561437f25c312b698d53088882
kashubhatkar <bhatkarkahkasha@gmail.com>
2025-06-21 21:25:18 +0530

    datatypes

---
+++ 01_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";// treat all js code as newer version
+
+//alert(3+3)// we are using node js,not browser
+console.log(3+3)//code readability should be high
+console.log("kahkasha")
+
+
+let name ="kahkasha"
+let age=3
+let isLoggedin=false
+//primitive datatypes
+// number
+//bigInt
+//string-"kahkasha"
+//boolean-yes/no
+//null-standalone value
+// symbol-unique
+
+console.log(typeof "kahkasha");
+console.log(typeof age);
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
