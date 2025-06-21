27716dcefdb620847eeac515cb93c8bb9f71ae29
kashubhatkar <113614663+kashubhatkar@users.noreply.github.com>
2025-06-21 21:42:23 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,33 @@
+// const id=12345
+// id =12
+// let age=24
+// age=25
+// // var name="kahkasha"
+
+// console.log(newid);
+// console.log(age);
+// // console.log(name);
+
+// var x=10;
+// var x=5;
+//  let y=12
+//  let a=13
+//  const z=13
+//  const m=15
+ 
+// function test(){
+//     let message="hello";
+//     console.log(message);
+// }
+// test()
+//     console.log(message);
+
+    
+if(true){
+    let size="small";
+    const color="red";
+    
+
+}
+ console.log(size);
+    console.log(color);
