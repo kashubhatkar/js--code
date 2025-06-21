15c800870711a18515ae1e41112302106e633654
kashubhatkar <bhatkarkahkasha@gmail.com>
2025-06-21 23:02:51 +0530

    datatypes

--- 01_datatypes.js
+++ 01_datatypes.js
@@ -1,14 +1,12 @@
-<<<<<<< HEAD
-"use strict";// treat all js code as newer version
+"use strict"; // treat all js code as newer version
 
 //alert(3+3)// we are using node js,not browser
-console.log(3+3)//code readability should be high
-console.log("kahkasha")
+console.log(3 + 3); //code readability should be high
+console.log("kahkasha");
 
-
-let name ="kahkasha"
-let age=3
-let isLoggedin=false
+let name = "kahkasha";
+let age = 3;
+let isLoggedin = false;
 //primitive datatypes
 // number
 //bigInt
@@ -19,28 +17,5 @@ let isLoggedin=false
 
 console.log(typeof "kahkasha");
 console.log(typeof age);
-console.log(typeof null);//object
-=======
-"use strict";// treat all js code as newer version
-
-//alert(3+3)// we are using node js,not browser
-console.log(3+3)//code readability should be high
-console.log("kahkasha")
-
-
-let name ="kahkasha"
-let age=3
-let isLoggedin=false
-//primitive datatypes
-// number
-//bigInt
-//string-"kahkasha"
-//boolean-yes/no
-//null-standalone value
-// symbol-unique
-
-console.log(typeof "kahkasha");
-console.log(typeof age);
-console.log(typeof null);//object
->>>>>>> 21157aee9f864baa0b311acf83ddff84c99a8487
-console.log(typeof undefined);//undefined
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined remove errors
