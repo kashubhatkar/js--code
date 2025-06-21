21157aee9f864baa0b311acf83ddff84c99a8487
kashubhatkar <113614663+kashubhatkar@users.noreply.github.com>
2025-06-21 22:24:37 +0530

    Add files via upload

---
+++ conversion operations.js
@@ -0,0 +1,30 @@
+// let score=null
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+
+//  let score1="33.5"
+// console.log(typeof score1);
+// let valueInNumber1 = Number(score1)
+// console.log(typeof valueInNumber1);
+// console.log(valueInNumber1)
+  
+//  let name="abc"
+// console.log(typeof name);
+// let valueInNumber12 = Number(name)
+// console.log(typeof valueInNumber12);
+// console.log(valueInNumber12)
+
+// let x=undefined
+// console.log(typeof x);
+// let valueInNumber13=Number(x)
+// console.log(typeof valueInNumber13);
+
+//boolean-true>1,false>0
+// ""-false
+//"kahkasha"-true 
+let isLoggedIn = "kahkasha"
+let booleanisloggedin = Boolean(isLoggedIn)
+console.log(booleanisloggedin)
